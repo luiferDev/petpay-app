2812ea7e6b7e7ebe8aec563393e1f81b362cd1f2
LuiferDev <luifer991@gmail.com>
2025-10-11 23:34:46 -0500

    change path of the schema

--- Identity/drizzle.config.ts
+++ Identity/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   out: './drizzle',
-  schema: './model/schema.ts',
+  schema: './src/infrastructure/persistence/schemas.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5432/postgres'
