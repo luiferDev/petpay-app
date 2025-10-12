91ea3ffdafaa290e4af19bae530abcf87797c0d7
LuiferDev <luifer991@gmail.com>
2025-10-11 23:21:38 -0500

    add: repository

---
+++ Identity/src/core/domain/ports/user.repository.ts
@@ -0,0 +1,16 @@
+// Define el CONTRATO: Lo que el caso de uso NECESITA
+
+import { AccountCreateInput, UserRegisterInput } from '../../../infrastructure/web/validations/register.validation'
+
+export interface IUserRepository {
+  /**
+     * Guarda un nuevo usuario y su cuenta en la persistencia.
+     */
+  registerUserWithAccount: (
+    userData: UserRegisterInput,
+    accountData: AccountCreateInput
+  ) => Promise<{ userId: string, accountId: number }>
+
+  // Otros métodos de acceso a datos (ej. findByEmail, findById)
+  findByEmail: (email: string) => Promise<any | null>
+}
