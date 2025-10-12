d9a4fb5ee7f2da366adabcd5bf5d94d103905f43
LuiferDev <luifer991@gmail.com>
2025-10-11 23:21:58 -0500

    add: conflct error log

---
+++ Identity/src/core/domain/ports/exceptions/ConflictError.ts
@@ -0,0 +1,15 @@
+// src/core/domain/exceptions/ConflictError.ts
+
+/**
+ * Excepción de dominio para errores de Conflicto (HTTP 409), 
+ * como un recurso que ya existe (ej. email duplicado).
+ */
+export class ConflictError extends Error {
+  public statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'ConflictError';
+    this.statusCode = 409;
+  }
+}
