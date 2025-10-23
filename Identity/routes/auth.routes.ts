33c3d4f56281809b8c94c2703999739770aebebe
LuiferDev <luifer991@gmail.com>
2025-10-22 22:57:21 -0500

    feature: login and users routers

--- Identity/routes/auth.routes.ts
+++ Identity/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers/auth-controller'
-import { AuthService } from '../services/register-service'
+import { AuthService } from '../services/auth-service'
 import { UserRepository } from '../model/user-repository'
 
 const router = Router()
@@ -8,5 +8,7 @@ const authService = new AuthService({ authRepository: UserRepository })
 const authController = new AuthController({ authService })
 
 router.post('/register', authController.create)
+router.post('/login', authController.login)
+router.get('/users', authController.listUsers)
 
 export default router
