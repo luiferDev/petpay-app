666c97417d6ed2335c86a9459200b49574642114
LuiferDev <luifer991@gmail.com>
2025-10-28 09:26:46 -0500

    feat: refactor EmailService to use IEmailTransport for sending verification emails

--- Identity/services/EmailService.ts
+++ Identity/services/EmailService.ts
@@ -1,8 +1,13 @@
 import { IEmailService } from '../interfaces/IEmailService'
-import { sendVerificationEmail } from '../utils/nodemailer'
+
+export interface IEmailTransport {
+  send(template: string, to: string, subject: string, locals?: Record<string, any>): Promise<any>
+}
 
 export class EmailService implements IEmailService {
+  constructor(private readonly transport: IEmailTransport) { }
+
   async sendVerificationEmail(email: string, firstName: string, verificationLink: string): Promise<void> {
-    await sendVerificationEmail(email, firstName, verificationLink)
+    await this.transport.send('verificationEmail', email, 'Verificación de Correo Electrónico', { firstName, verificationLink })
   }
 }
