65e03ee0e8824a766514ad3eba24aa9eb7299b95
LuiferDev <luifer991@gmail.com>
2025-10-27 20:08:12 -0500

    feat: enhance user registration by implementing role-based registration logic

--- Identity/routes/auth.routes.ts
+++ Identity/routes/auth.routes.ts
@@ -11,5 +11,6 @@ router.post('/register', authController.createClient)
 router.post('/login', authController.login)
 router.get('/users', authController.listUsers)
 router.get('/verify/:userId', authController.verifyEmail)
+router.post('/register/:role', authController.registerByRole)
 
 export default router
