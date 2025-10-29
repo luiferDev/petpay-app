6646ac51d59bcd56992c13ed542974d6e91c3b90
LuiferDev <luifer991@gmail.com>
2025-10-29 15:29:11 -0500

    add: new interface to type outputs and create enum for roles

--- Identity/interfaces/IUserRepository.ts
+++ Identity/interfaces/IUserRepository.ts
@@ -1,6 +1,5 @@
 import { LoginInput } from '../schema/login-schema'
 import { UserRegisterInput, AccountCreateInput } from '../schema/register-schema'
-import { Role } from '../template-method/register.template'
 
 export interface IUser {
   id: string
@@ -14,16 +13,27 @@ export interface IUser {
   updatedAt: Date
 }
 
-export interface IUserRole {
-  userId: string
-  role: Role
+export interface User {
+  id: string
+  email: string
+  firstName: string
+  lastName: string
+  phone?: string | null
+  isVerified: boolean
+  role: { UserRole: UserRole }
+}
+
+export enum UserRole {
+  CLIENT = 'CLIENT',
+  SERVICE_PROVIDER = 'SERVICE_PROVIDER',
+  ADMIN = 'ADMIN'
 }
 
 export interface IUserRepository {
   registerClient(userData: UserRegisterInput, accountData: AccountCreateInput): Promise<{ userId: string, accountId: number }>
   registerServiceProvider(userData: UserRegisterInput, accountData: AccountCreateInput): Promise<{ userId: string, accountId: number }>
   registerAdmin(userData: UserRegisterInput, accountData: AccountCreateInput): Promise<{ userId: string, accountId: number }>
-  loginUser(loginData: LoginInput): Promise<{ user: IUser, role: IUserRole | null }>
-  getAllUsers(): Promise<Pick<IUser, 'id' | 'email'>[]>
-  verifyUser(userId: string): Promise<IUser>
+  loginUser(loginData: LoginInput): Promise<{ user: IUser, role: UserRole }>
+  getAllUsers(): Promise<{ users: User[] }>
+  verifyUser(userId: string): Promise<{ id: string, email: string, isVerified: boolean }>
 }
