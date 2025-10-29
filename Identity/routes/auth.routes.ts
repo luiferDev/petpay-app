29164ee9d8703bf4d9eb7af512edb67d58abb9d7
LuiferDev <luifer991@gmail.com>
2025-10-29 15:33:59 -0500

    remove container and lazy loading

--- Identity/routes/auth.routes.ts
+++ Identity/routes/auth.routes.ts
@@ -1,15 +1,25 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import { AuthController } from '../controllers/auth-controller'
-import { DIContainer } from '../container/DIContainer'
+import { AuthService } from '../services/auth-service'
+import { EmailService } from '../services/EmailService'
+import { Db } from '../model/schema'
+import { createNodemailerTransport } from '../utils/nodemailer'
+import { DrizzleUserRepository } from '../model/drizzle.user.repository'
+
 
 const router = Router()
-const container = DIContainer.getInstance()
-const authController = container.get<AuthController>('AuthController')
 
-router.post('/register', authController.createClient)
-router.post('/login', authController.login)
-router.get('/users', authController.listUsers)
-router.get('/verify/:userId', authController.verifyEmail)
-router.post('/register/:role', authController.registerByRole)
+// Crear dependencias manualmente
+const userRepository = new DrizzleUserRepository(Db)
+const emailTransport = createNodemailerTransport()
+const emailService = new EmailService(emailTransport)
+const authService = new AuthService(userRepository, emailService)
+const authController = new AuthController(authService)
+
+router.post('/register', (req: Request, res: Response) => authController.createClient(req, res))
+router.post('/login', (req: Request, res: Response) => authController.login(req, res))
+router.get('/users', (req: Request, res: Response) => authController.listUsers(req, res))
+router.get('/verify/:userId', (req: Request, res: Response) => authController.verifyEmail(req, res))
+router.post('/register/:role', (req: Request, res: Response) => authController.registerByRole(req, res))
 
 export default router
