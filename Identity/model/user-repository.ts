98b097a568162f525fb298d867601aa434295407
LuiferDev <luifer991@gmail.com>
2025-10-29 15:36:57 -0500

    TODO: make with prisma, mongo or delete it

--- Identity/model/user-repository.ts
+++ Identity/model/user-repository.ts
@@ -7,11 +7,14 @@ import { logger } from '../lib/logger'
 import { ClientUserRegister } from '../template-method/concrete-classes/ClientUserRegister'
 import { AdminUserRegister } from '../template-method/concrete-classes/AdminUserRegister'
 import { ServiceProviderUserRegister } from '../template-method/concrete-classes/ServiceProviderUserRegister'
-import { Role } from '../template-method/register.template'
-import { IUserRepository, IUser, IUserRole } from '../interfaces/IUserRepository'
+import { IUserRepository, IUser, UserRole, User } from '../interfaces/IUserRepository'
+import { Db } from './schema'
 
 export class UserRepository implements IUserRepository {
-  constructor(private readonly db: any) { }
+  drizzle = Db
+  constructor(database: typeof Db) {
+    this.drizzle = database
+  }
 
   /**
      * Registra un nuevo usuario, crea su cuenta asociada y establece los roles iniciales,
@@ -24,7 +27,7 @@ export class UserRepository implements IUserRepository {
     userData: UserRegisterInput,
     accountData: AccountCreateInput
   ): Promise<{ userId: string, accountId: number }> {
-    const clientRegister = new ClientUserRegister(this.db)
+    const clientRegister = new ClientUserRegister(this.drizzle)
     const result = await clientRegister.registerUser(userData, accountData)
     return { userId: result.userId, accountId: result.accountId }
   }
@@ -33,7 +36,7 @@ export class UserRepository implements IUserRepository {
     userData: UserRegisterInput,
     accountData: AccountCreateInput
   ): Promise<{ userId: string, accountId: number }> {
-    const serviceProviderUserRegister = new ServiceProviderUserRegister(this.db)
+    const serviceProviderUserRegister = new ServiceProviderUserRegister(this.drizzle)
     const result = await serviceProviderUserRegister.registerUser(userData, accountData)
     return { userId: result.userId, accountId: result.accountId }
   }
@@ -42,28 +45,31 @@ export class UserRepository implements IUserRepository {
     userData: UserRegisterInput,
     accountData: AccountCreateInput
   ): Promise<{ userId: string, accountId: number }> {
-    const adminUserRegister = new AdminUserRegister(this.db)
+    const adminUserRegister = new AdminUserRegister(this.drizzle)
     const result = await adminUserRegister.registerUser(userData, accountData)
     return { userId: result.userId, accountId: result.accountId }
   }
 
-  public async loginUser({ email, passwordHash }: LoginInput): Promise<{ user: IUser, role: IUserRole | null }> {
+  public async loginUser({ email, passwordHash }: LoginInput): Promise<{ user: IUser, role: UserRole }> {
     logger.info('Buscando usuario', { email })
-    const [user] = await this.db.select().from(users)
+    const [user] = await this.drizzle.select().from(users)
       .where(eq(users.email, email))
-    const [role] = await this.db.select({ userId: userRoles.userId, role: userRoles.role }).from(users)
+    const [userRole] = await this.drizzle.select().from(users)
       .rightJoin(userRoles, eq(users.id, userRoles.userId))
       .where(eq(users.email, email))
 
+    const role = userRole?.user_roles.role
+
     if (!user) {
       logger.warn('Usuario no encontrado', { email })
-      const allUsers = await this.db.select({ email: users.email }).from(users)
+      const allUsers = await this.drizzle.select({ email: users.email }).from(users)
       logger.debug('Usuarios en la base de datos', { count: allUsers.length })
       throw new Error('Usuario no existe')
     }
 
-    if (!role) {
-      logger.warn('Usuario sin rol asignado', { userId: user.id })
+    if (role == null || !(role in UserRole)) {
+      logger.warn('Usuario sin rol asignado o inválido', { userId: user.id, role })
+      throw new Error('Usuario sin rol asignado')
     }
 
     logger.info('Usuario encontrado, verificando contraseña')
@@ -75,15 +81,34 @@ export class UserRepository implements IUserRepository {
     }
 
     logger.info('Login exitoso', { userId: user.id })
-    return { user: user as IUser, role: role ? { userId: role.userId, role: role.role as Role } : null }
+    return { user: user, role: UserRole[role] }
   }
 
-  public async getAllUsers(): Promise<Pick<IUser, 'id' | 'email'>[]> {
-    return await this.db.select({ id: users.id, email: users.email }).from(users)
+  public async getAllUsers(): Promise<{ users: User[] }> {
+    logger.info('Obteniendo todos los usuarios')
+    const usersData = await this.drizzle.select({ 
+      id: users.id, 
+      email: users.email, 
+      firstName: users.firstName, 
+      lastName: users.lastName, 
+      isVerified: users.isVerified,
+      role: userRoles.role
+    }).from(users).leftJoin(userRoles, eq(users.id, userRoles.userId))
+
+    const mappedUsers: User[] = usersData.map(user => ({
+      id: user.id!,
+      email: user.email!,
+      firstName: user.firstName!,
+      lastName: user.lastName!,
+      isVerified: user.isVerified!,
+      role: { UserRole: UserRole[user.role!] }
+    }))
+
+    return { users: mappedUsers }
   }
 
   public async verifyUser(userId: string): Promise<IUser> {
-    const [updatedUser] = await this.db.update(users)
+    const [updatedUser] = await this.drizzle.update(users)
       .set({ isVerified: true, updatedAt: new Date() })
       .where(eq(users.id, userId))
       .returning()
