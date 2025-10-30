389f69f919baa4f8b2bae6850ff214e7bba58a54
LuiferDev <luifer991@gmail.com>
2025-10-29 21:18:47 -0500

    reformat

--- Identity/model/schema.ts
+++ Identity/model/schema.ts
@@ -21,6 +21,7 @@ import { Pool } from 'pg'
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 })
+
 export const Db = drizzle({ client: pool })
 // ----------------------------------------
 
