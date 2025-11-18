ece5f4788cfcfd0ed10b66adfe0b57585d99193d
LuiferDev <luifer991@gmail.com>
2025-11-18 14:33:53 -0500

    add: find all by category

---
+++ catalog-&-offers/internal/infrastructure/http/routes.go
@@ -0,0 +1,17 @@
+package http
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter(controller *Controller) *gin.Engine {
+	r := gin.Default()
+	api := r.Group("/api/v1")
+	{
+		api.GET("/products/:id", controller.GetOneById)
+		api.GET("/products/all", controller.GetAll)
+		api.GET("/products", controller.FindAllByCategory)
+		api.POST("/products", controller.Create)
+		api.PATCH("/products/:id", controller.Update)
+		api.DELETE("/products/:id", controller.Delete)
+	}
+	return r
+}
