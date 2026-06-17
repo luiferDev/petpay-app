460f4fb57ca3647acc99449241cc0a8913002ee3
luiferDev <luifer991@gmail.com>
2026-06-16 20:05:46 -0500

    chore: add postgres init script for payments service
    - Add init.sql to create petpay_payments database

---
+++ postgres/init.sql
@@ -0,0 +1,6 @@
+-- Create separate databases for each microservice
+CREATE DATABASE petpay_identity;
+CREATE DATABASE petpay_marketplace;
+CREATE DATABASE petpay_catalog;
+CREATE DATABASE petpay_bookings;
+CREATE DATABASE petpay_payments;
